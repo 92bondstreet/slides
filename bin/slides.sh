1a233b1ae1040a95f7f1f368c1a530d2e9078b8d
Yassine AZZOUT <yassine.azzout@gmail.com>
2019-03-19 14:54:09 +0100

    chore(slides): display useful commands

--- bin/slides.sh
+++ bin/slides.sh
@@ -5,6 +5,16 @@ bootstrap () {
   curl -o reveal.html https://raw.githubusercontent.com/92bondstreet/slides/master/reveal.html
   curl -o reveal.css https://raw.githubusercontent.com/92bondstreet/slides/master/reveal.css
   curl -o reveal.json https://raw.githubusercontent.com/92bondstreet/slides/master/reveal.json
+  curl -o robots.txt https://raw.githubusercontent.com/92bondstreet/slides/master/robots.txt
+}
+
+useful () {
+  echo "⌨️  Useful command to save"
+  echo "## start reveal on localhost"
+  echo "❯ reveal-md SLIDES.md --css reveal.css --template reveal.html -w"
+  echo "## build and deploy reveal slides"
+  echo "❯ reveal-md SLIDES.md --css reveal.css --template reveal.html --static dist"
 }
 
 bootstrap
+useful
